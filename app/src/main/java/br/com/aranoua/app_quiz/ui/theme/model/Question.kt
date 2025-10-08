942d46f4c8f5a9ad7245c7ef2819bd8d696baad2
fabriciocoelho <fabriciocoelho03082000@hotmail.com>
2025-10-08 11:15:33 -0400

    feat: Cria o modelo de dados `Question`
    Adiciona a data class `Question` para representar a estrutura de uma pergunta do quiz.
    
    A classe inclui os seguintes campos:
    - `pergunta`: O texto da pergunta.
    - `opcoes`: Uma lista de opções de resposta.
    - `correta`: A resposta correta.
    - `categoria`: A categoria da pergunta.
    - `curiosidade`: Um fato interessante relacionado à pergunta.

--- app/src/main/java/br/com/aranoua/app_quiz/ui/theme/model/Question.kt
+++ app/src/main/java/br/com/aranoua/app_quiz/ui/theme/model/Question.kt
@@ -1,2 +1,9 @@
 package br.com.aranoua.app_quiz.ui.theme.model
 
+data class Question(
+    val pergunta: String,
+    val opcoes: List<String>,
+    val correta: String,
+    val categoria: String,
+    val curiosidade: String
+)
